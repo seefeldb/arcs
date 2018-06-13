830530da3fab652e4e71d329e30cdc1c63e96a94
Bernhard A. M. Seefeld <seefeld@gmail.com>
2018-06-12 17:11:47 -0700

    Simple service worker, set up to flush cache on reload

--- shell/sw.js
+++ shell/sw.js
@@ -1,19 +1,27 @@
-// simple cache, flushing it on reload (if loaded via shell/apps/...)
+// simple cache
+// clears if receiving fetch for ../flush.me
+// must be in shell/ so it can cache shell/artifacts/, etc.
 
-const cacheName = 'arcs';
+let cacheVersion = 1;
+
+self.addEventListener('install', function(event) {
+  console.log('cache: install');
+});
 
 self.addEventListener('fetch', function(event) {
-  if (event.request.url.match(/\/apps\/[a-z]+\/(index.html|\?|$)/)) {
+  const cacheName = 'cache' + cacheVersion;
+  if (event.request.url.match(/\/flush\.me$/)) {
     event.waitUntil(caches.delete(cacheName));
-    event.respondWith(fetch(event.request));
-    console.log(`cache: flushed (trigger was [${event.request.url}]`);
+    event.respondWith(new Response(`cache $cacheName flushed.`));
+    cacheVersion++;
+    console.log('cache: flushed, and version incremented to', cacheVersion);
   } else event.respondWith(
     caches.match(event.request).then(function(resp) {
       return resp || fetch(event.request).then(function(response) {
         return caches.open(cacheName).then(function(cache) {
           cache.put(event.request, response.clone());
           return response;
-        });
+        });  
       });
     })
   );
